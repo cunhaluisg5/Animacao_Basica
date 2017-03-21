6a463c9ecfde10b0066fad9d026e93a68057f6d3
luis <luisg5@yahoo.com.br>
2017-03-21 11:43:58 -0300

    Move npcs para Level

--- Level.js
+++ Level.js
@@ -1,14 +1,34 @@
 function Level(){
-	this.enemies = 1;
-	this.number = 1;
+  this.enemies = [];
+  this.number  = 1;
+  this.maxEnemies  = 1;
 
-	this.desenhar = function(ctx){
-		ctx.fillStyle = "yellow";
-		ctx.strokeStyle = "black"; //Contornar de preto
-		ctx.font = "1em Arial Black"; //Tamanho da fonte
-		var texto = "Level:"+ this.number + " Enemies:" +this.enemies;
-		ctx.fillText(texto, 100, 20);
-		ctx.strokeText(texto, 100, 20);
-	}
+  this.desenhar = function(ctx){
+    for (var i = 0; i < this.enemies.length; i++) {
+      this.enemies[i].desenhar(ctx);
+    }
+
+    ctx.fillStyle = "yellow";
+    ctx.strokeStyle = "black";
+    ctx.font = "1em Arial Black";
+    var texto = "Level: " + this.number + " Enemies: " +this.enemies;
+    ctx.fillText(texto, 100, 20);
+    ctx.strokeText(texto, 100, 20);
+  }
+
+  this.mover = function(dt){
+    for (var i = 0; i < this.enemies.length; i++) {
+      this.enemies[i].mover(dt);
+    }
+  }
+
+  this.inicia = function(){
+    for (var i = 0; i < this.maxEnemies; i++) {
+      var novoInimigo =  new Sprite();
+      novoInimigo.x = 200;
+      novoInimigo.y = 50+i*20;
+      novoInimigo.color = "red";
+      this.enemies.push(novoInimigo);
+    }
+  }
 }
-
